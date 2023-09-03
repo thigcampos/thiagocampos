f6e0ce0fff8bc62ba666757c08926a7eac91ca50
Thiago Campos <thigcampos@proton.me>
2023-09-03 20:35:45 -0300

    feat: minor updates in all pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,7 @@ function PostCard(post: Post) {
   )
 }
 
-export default function Home() {
+export default function Blog() {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,22 @@
-import Image from 'next/image'
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section>
+        {/*HEADER*/}
+      </section>
+      <section>
+        <div className={styles.headline}>
+          <h1 className={styles.title}>Salut, je suis Thiago Campos — <br/>
+          a software engineer based in Brazil 🇧🇷</h1>
+          <p className={styles.subtitle}>Check some my <a href='/blog'>[ blog ]</a> and some cool facts <a href='/about'>[ about me ]</a></p>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="/blog"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Blog <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="/about"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            About Me <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section>
+        {/*FOOTER*/}
+      </section>
     </main>
   )
 }
+

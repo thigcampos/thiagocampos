784f537615db4a16c01e4626089eab74a75d39e8
Thiago Campos <thigcampos@proton.me>
2023-09-04 17:50:50 -0300

    styles: fix typography styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </section>
       <section>
         <div className={styles.headline}>
-          <h1 className={styles.title}>Salut, je suis Thiago Campos — <br/>
+          <h1 className={styles.title}>Salut, je suis <span>Thiago Campos</span> — <br/>
           a software engineer based in Brazil 🇧🇷</h1>
           <p className={styles.subtitle}>Check some my <a href='/blog'>[ blog ]</a> and some cool facts <a href='/about'>[ about me ]</a></p>
         </div>

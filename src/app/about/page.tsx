a25dc182c76f07ecad6134db3bee67deff862cc7
Thiago Campos <thigcampos@proton.me>
2023-09-05 11:07:56 -0300

    style: update general look and feel

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,9 +14,10 @@ export default function Home() {
             <div className={styles.content}>
               <span className={styles.date}>February, 11 2023 | 16:14</span>
               <p className={styles.text}>
-                Hey! I'm Thiago Campos, a software engineer and design enthusiast based in brazil. 
+                Hey! I'm <span>Thiago Campos</span>, a software engineer and design enthusiast based in <span>Brazil</span>. 
                 Currently, I'm working on developing and maintaining React Application for a brazilian software house, 
-                I'm also co-founder, lead designer and front-end developer in [ scopo studio ].
+                I'm also co-founder, lead designer and front-end developer in <span>Scopo Studio</span>.
+                <br/><br/>
                 I've always been inspired by the tech industry, computers and software. 
                 In my spare time I explore adjacent areas to software development, mainly design and hardware.
               </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <section>
         <div className={styles.headline}>
           <h1 className={styles.title}>Salut, je suis <span>Thiago Campos</span> — <br/>
-          a software engineer based in Brazil 🇧🇷</h1>
-          <p className={styles.subtitle}>Check some my <a href='/blog'>[ blog ]</a> and some cool facts <a href='/about'>[ about me ]</a></p>
+          a software engineer based in Brazil</h1>
+          <p className={styles.subtitle}>Check my <a href='/blog'>blog</a> and some cool facts <a href='/about'>about me</a></p>
         </div>
       </section>
       <Footer />

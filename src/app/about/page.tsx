95811aab68c63105e29c871bfe06ff731bb67eda
Thiago Campos <thigcampos@proton.me>
2023-09-05 13:56:17 -0300

    fix: replaced apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
             <div className={styles.content}>
               <span className={styles.date}>February, 11 2023 | 16:14</span>
               <p className={styles.text}>
-                Hey! I'm <span>Thiago Campos</span>, a software engineer and design enthusiast based in <span>Brazil</span>. 
-                Currently, I'm working on developing and maintaining React Application for a brazilian software house, 
-                I'm also co-founder, lead designer and front-end developer in <span>Scopo Studio</span>.
+                Hey! I&apos;m <span>Thiago Campos</span>, a software engineer and design enthusiast based in <span>Brazil</span>. 
+                Currently, I&apos;m working on developing and maintaining React Application for a brazilian software house, 
+                I&apos;m also co-founder, lead designer and front-end developer in <span>Scopo Studio</span>.
                 <br/><br/>
-                I've always been inspired by the tech industry, computers and software. 
+                I&apos;ve always been inspired by the tech industry, computers and software. 
                 In my spare time I explore adjacent areas to software development, mainly design and hardware.
               </p>
             </div>

c4ef305243aa87cab5274fa3c5527588a5f19a04
Thiago Campos <thigcampos@proton.me>
2023-09-06 18:08:00 -0300

    feat: update post card link scope

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -5,16 +5,14 @@ import { Post } from 'contentlayer/generated'
 
 export default function PostCard(post: Post) {
     return (
-      <div className={styles.container}>
-        <h2 className={styles.title}>
-          <Link href={post.url}>
-            {post.title}
-          </Link>
-        </h2>
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.description }} />
-        <time dateTime={post.date} className={styles.date}>
-          {format(parseISO(post.date), 'LLLL d, yyyy')}
-        </time>
-      </div>
+      <Link href={post.url} className={styles.container}>
+          <h2 className={styles.title}>
+              {post.title}
+          </h2>
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.description }} />
+          <time dateTime={post.date} className={styles.date}>
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time>
+      </Link>
     )
   }

76adcb0c7fb11400800c9118b8b3379d95c25ed8
Thiago Campos <thigcampos@proton.me>
2023-09-09 00:26:53 -0300

    fix: add sharp to compress and resolve apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,16 +16,16 @@ export default function Home() {
             <div className={styles.content}>
               <span className={styles.date}>September, 08 2023 | 19:09</span>
                 <p className={styles.text}>
-                  Hey there! 👋 I'm <span>Thiago Campos</span>, a software engineer based in <span>Brazil</span> 🇧🇷. 
-                  Currently, I'm working as a React Developer at Pride Innovations, where I'm deeply involved in crafting in-house systems. 
+                  Hey there! 👋 I&apos;m <span>Thiago Campos</span>, a software engineer based in <span>Brazil</span> 🇧🇷. 
+                  Currently, I&apos;m working as a React Developer at Pride Innovations, where I&apos;m deeply involved in crafting in-house systems. 
                   All my devices are powered by Linux, from my Android smartphone to Fedora running on my home server, desktop, and laptop.
                 </p>
                 <br/><br/>
                 {/* ADD LINKS TO GITHUB AND SIDE-PROJECTS */}
                 <br/>
                 <p className={styles.text}>
-                  Besides coding, I'm a design enthusiast, primarily inspired by <span>fashion</span> and <span>architecture</span>. 
-                  While savoring a good <span>home-brewed coffee</span>, you'll often find me immersed in the smooth rhythms of <span>Bossa Nova</span> and <span>Jazz</span>. 
+                  Besides coding, I&apos;m a design enthusiast, primarily inspired by <span>fashion</span> and <span>architecture</span>. 
+                  While savoring a good <span>home-brewed coffee</span>, you&apos;ll often find me immersed in the smooth rhythms of <span>Bossa Nova</span> and <span>Jazz</span>. 
                   Feel free to check out some of my Spotify playlists filled with handpicked treasures.
                 </p>
                 <br/><br/>
@@ -35,9 +35,9 @@ export default function Home() {
                 </div>
                 <br/><br/>
                 <p className={styles.text}>
-                  Beyond the screen, you'll find me watching movies, <span>cooking up some pasta</span>, or embarking on new adventures. 
-                  I've had the privilege of exploring various <span>Brazilian cities</span>, each with its unique charm and culture. 
-                  Additionally, I've ventured beyond my homeland, visiting captivating countries in Europe like <span>Spain, France, 
+                  Beyond the screen, you&apos;ll find me watching movies, <span>cooking up some pasta</span>, or embarking on new adventures. 
+                  I&apos;ve had the privilege of exploring various <span>Brazilian cities</span>, each with its unique charm and culture. 
+                  Additionally, I&apos;ve ventured beyond my homeland, visiting captivating countries in Europe like <span>Spain, France, 
                   Italy and Monaco</span>, which have enriched my life with diverse experiences and cultural insights.
                 </p>
                 <br/>

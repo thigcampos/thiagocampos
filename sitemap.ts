33d2f685b34fe638faf98b4cc92451947e945db6
Thiago Campos <thigcampos@proton.me>
2023-09-09 12:35:21 -0300

    docs: add robots.txt and sitemap

---
+++ sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://thigcampos.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://thigcampos.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://thigcampos.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}

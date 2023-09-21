723e29e2f489e4a2f9ba681734a838b41664f65f
Thiago Campos <thigcampos@proton.me>
2023-09-20 22:21:05 -0300

    blog: add a new wip-post and start to set tags

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -8,6 +8,7 @@ export const Post = defineDocumentType(() => ({
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
     description: { type: 'string', required: true },
+    tag: { type: 'string' }
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/blog/${post._raw.flattenedPath}` },

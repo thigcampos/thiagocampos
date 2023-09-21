42151483155c2f6feaa184d888651d147731f006
Thiago Campos <thigcampos@proton.me>
2023-09-20 22:21:18 -0300

    feat: add a rss icon

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,8 @@ import { compareDesc } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
 import { Footer, Header, PostCard } from '@/components';
 import { blogNavigation } from '@/constants/navigations';
+import Icon from '@mdi/react';
+import { mdiAt, mdiGithub, mdiInstagram, mdiPinterest, mdiReddit, mdiRss, mdiSpotify } from '@mdi/js';
 import styles from './blog.module.css';
 
 export default function Blog() {
@@ -11,7 +13,10 @@ export default function Blog() {
       <div className={styles.highlight}>
         <Header navGroup={blogNavigation}/>
         <section>
-          <h1 className={styles.title}>Blog</h1>
+          <h1 className={styles.title}>
+            Blog
+            <Icon path={mdiRss} />
+          </h1>
           <div className={styles.postsWrapper}>
             {posts.map((post, idx) => (
               <PostCard key={idx} {...post} />

7acbd935292db9d1e6bfea1a0f098427fc5f8945
Thiago Campos <thigcampos@proton.me>
2023-09-27 20:25:15 -0300

    feat: add rss feed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,6 @@ const playfairItalic = Playfair_Display({
   style: 'italic'
 });
 
-export const metadata: Metadata = {
-  title: 'Thiago Campos | Software Engineer',
-  description: `Hey there! I am Thiago Campos, a software engineer based in Brazil. 
-  Currently, I am working as a React Developer at Pride Innovations, where I am deeply involved in crafting in-house systems.`,
-};
-
 export default function RootLayout({
   children,
 }: {
